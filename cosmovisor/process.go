14f6b42668f9baa3722026cdd30cd8d141589044
anilCSE <anil@vitwit.com>
2021-07-15 00:20:35 +0530

    cleanup merge issues

--- cosmovisor/process.go
+++ cosmovisor/process.go
@@ -8,7 +8,6 @@ import (
 	"os/exec"
 	"os/signal"
 	"strings"
-	"sync"
 	"syscall"
 	"time"
 
@@ -83,34 +82,6 @@ func doBackup(cfg *Config) error {
 	}
 
 	return nil
-}
-
-// WaitResult is used to wrap feedback on cmd state with some mutex logic.
-// This is needed as multiple go-routines can affect this - two read pipes that can trigger upgrade
-// As well as the command, which can fail
-type WaitResult struct {
-	// both err and info may be updated from several go-routines
-	// access is wrapped by mutex and should only be done through methods
-	err   error
-	info  *UpgradeInfo
-	mutex sync.Mutex
-}
-
-// AsResult reads the data protected by mutex to avoid race conditions
-func (u *WaitResult) AsResult() (*UpgradeInfo, error) {
-	u.mutex.Lock()
-	defer u.mutex.Unlock()
-	return u.info, u.err
-}
-
-// SetError will set with the first error using a mutex
-// don't set it once info is set, that means we chose to kill the process
-func (u *WaitResult) SetError(myErr error) {
-	u.mutex.Lock()
-	defer u.mutex.Unlock()
-	if u.info == nil && myErr != nil {
-		u.err = myErr
-	}
 }
 
 // WaitForUpgradeOrExit checks upgrade plan file created by the app.
